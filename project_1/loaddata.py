55269341c7452fc06855d66aa78d149fc992279e
Brian Lange <brian.lange@datascopeanalytics.com>
2015-09-15 13:17:58 -0400

    Added in-class version of loaddata

--- project_1/loaddata.py
+++ project_1/loaddata.py
@@ -6,10 +6,30 @@ CapitalOne Metis Data Science Python Bootcamp Pilot Extravaganza 2K15.
 
 # imports
 import os
+import json
+import pprint
 
 # constants
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(CURRENT_DIR, 'data'))
 MOJO_DIR = os.path.join(DATA_DIR, 'boxofficemojo')
 
+def get_boxofficemojo_movies():
+    file_contents = os.listdir(MOJO_DIR)
 
+    movie_list = []
+
+    for filename in file_contents:
+        filepath = os.path.join(MOJO_DIR, filename)
+
+        with open(filepath, 'r') as movie_file:
+            movie_data = json.load(movie_file)
+
+        movie_list.append(movie_data)
+
+    print "Parsed %i movies from %i files" % (len(movie_list),
+                                              len(file_contents))
+    return movie_list
+
+if __name__ == "__main__":
+    movies = get_boxofficemojo_movies()
