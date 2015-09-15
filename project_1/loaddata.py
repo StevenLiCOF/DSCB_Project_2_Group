3b85a2886378cb32f2aedbdbf16b5eff6470848f
Brian Lange <brian.lange@datascopeanalytics.com>
2015-09-15 01:54:38 -0400

    add starting place for loaddata

--- project_1/loaddata.py
+++ project_1/loaddata.py
@@ -1,1 +1,14 @@
-__author__ = 'brian'
+"""loaddata is a module for accessing scraped data about movies from
+BoxOfficeMojo and Metacritic.
+It's built specifically to work with the data collected for the
+CapitalOne Metis Data Science Python Bootcamp Pilot Extravaganza 2K15.
+"""
+
+# imports
+import os
+
+# constants
+DATA_DIR = os.path.abspath('data')
+MOJO_DIR = os.path.join(DATA_DIR, 'boxofficemojo')
+
+
