f0b549f76c7b10de8b1890a46b66626f7ebb4191
Steven Li <steven.li@capitalone.com>
2015-09-22 10:27:27 -0400

    Pair exercise for day 7

--- day_07/pair.py
+++ day_07/pair.py
@@ -7,5 +7,23 @@ denominations = [(.01, "penny"),
                  (10, "ten dollar bill"),
                  (20, "twenty dollar bill")]
 
+denominations=dict(denominations)
 
+def calculateChange(price, given):
+    price=float(price)
+    given=float(given)
+    if given<price:
+        return "Not enough money"
+    changemoney={}
+    changeamount=given-price
+    for denom in sorted(denominations.keys(),reverse=True):
+        if int(changeamount/denom)>0:
+            changemoney[denominations[denom]]=int(changeamount/denom)
+        changeamount=round(changeamount % denom, 2)
+        #changemoney=sorted(changemoney,key=denominations)
+    return changemoney
 
+print calculateChange(20, 20.00)
+print calculateChange(14.28, 20.00)
+print calculateChange(21, 20.00)
+print calculateChange(0, 20.00)
