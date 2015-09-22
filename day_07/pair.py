904e99a683d4603786e2c9a7e2c6d908558d5019
Brian Lange <brian.lange@datascopeanalytics.com>
2015-09-21 22:26:43 -0400

    added day 7 pair stuff

---
+++ day_07/pair.py
@@ -0,0 +1,11 @@
+denominations = [(.01, "penny"),
+                 (.05, "nickel"),
+                 (.10, "dime"),
+                 (.25, "quarter"),
+                 (1, "dollar bill"),
+                 (5, "five dollar bill"),
+                 (10, "ten dollar bill"),
+                 (20, "twenty dollar bill")]
+
+
+
